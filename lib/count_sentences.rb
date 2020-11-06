423fff72a7078e9ee6174d41e3379e24765a91c7
Nikolaus Riggi <nikolaus.riggi@gmail.com>
2020-11-05 20:35:00 -0800

    commit tree 9d0c76e8ba83635732d2c77031107fd7968dac9b

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,23 @@
 require 'pry'
 
 class String
-
+#returns true if ending in a .
   def sentence?
-    
+    self.end_with?(".")
   end
 
+#return true if a question
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
+#dontfully understand why this works but it works 
   def count_sentences
-
+    self.split(/[.!?]/).reject {|x| x.empty?}.size
+    
   end
 end
